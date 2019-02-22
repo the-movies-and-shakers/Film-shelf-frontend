9ee970275568f8cd01479c9484d6fbc3b5c21098
nanbob <hannahgrey1987@hotmail.co.uk>
2019-02-22 14:47:08 +0000

    Updated FilmTitleInputBox

--- src/components/FilmTitleInputBox.js
+++ src/components/FilmTitleInputBox.js
@@ -8,6 +8,9 @@ class FilmTitleInputBox extends React.Component {
                 <div className="col" style={styles.inputBox}>
                     <input class="form-control" type="text" placeholder="Add new film"/>
                 </div>
+                <div className="col" style={styles.button}>
+                    <button type="button" class="btn btn-secondary btn-lg">Add film</button>
+                </div>
             </div>
         );
     }
@@ -18,14 +21,19 @@ const styles = {
     inputBox: {
         textAlign: "center",
         float: "left",
-        paddingLeft: "50px",
-        paddingRight: "250px"
+        align: "center"
 
     },
     filmEntry: {
         textAlign: "center",
         marginTop: "50px",
+        paddingLeft: "50px"
+    },
+
+    button: {
+        textAlign: "center"
     }
+
 }
 
 export default FilmTitleInputBox;

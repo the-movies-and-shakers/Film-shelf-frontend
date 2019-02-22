04bc512152e0907021b634ed457927f91ab807f7
emmastockton <emmastockton10@gmail.com>
2019-02-22 11:19:01 +0000

    settingUpFrontEnd - all components set up

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class Header extends React.Component {
+
+
+}
+
+const styles = {
+    
+};
+
+export default Header;
---
+++ src/components/RatingInput.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class RatingInput extends React.Component {
+
+
+}
+
+const styles = {
+    
+};
+
+export default RatingInput;

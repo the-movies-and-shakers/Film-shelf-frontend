639d1efe035e055a6cd61335cb6cd9efd5d0a50d
nanbob <hannahgrey1987@hotmail.co.uk>
2019-02-22 12:30:09 +0000

    Updated header

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,11 +2,22 @@ import React from 'react';
 
 class Header extends React.Component {
 
-
+render() {
+    return (
+        <div className="container">
+            <h1 style={styles.header}>Film Shelf</h1>
+        </div>
+        );
+    }
 }
 
 const styles = {
-    
-};
+    header: {
+        color: "mediumslateblue",
+        fontWeight: "bold",
+        textAlign: "Center",
+        fontSize: "50px"
+    }
+}
 
 export default Header;

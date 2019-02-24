0982628c593810fbacb8b382cd08774915f5f5c2
emmastockton <emmastockton10@gmail.com>
2019-02-24 10:46:03 +0000

    Star ratings created - not yet styled

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import Header from './components/Header'
 import FilmTitleInputBox from './components/FilmTitleInputBox'
+import RatingInput from './components/RatingInput';
 
 class App extends Component {
   render() {
@@ -9,6 +10,7 @@ class App extends Component {
       <div className="App">
         <Header /> 
         <FilmTitleInputBox />
+        <RatingInput />
       </div>
     );
   }

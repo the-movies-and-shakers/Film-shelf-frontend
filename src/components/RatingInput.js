d3c197d628492bfe5c3950dfd85ac223cb093035
emmastockton <emmastockton10@gmail.com>
2019-02-25 20:21:24 +0000

    onStarHover is now working - some comments in code to explain how I want it to work - not yet done

--- src/components/RatingInput.js
+++ src/components/RatingInput.js
@@ -7,16 +7,19 @@ class RatingInput extends React.Component {
     super();
 
     this.state = {
-      rating: 0
+      rating: 0,
+      clicked: false
     };
   }
 
   onStarClick(nextValue, prevValue, name) {
-
+    //this sets the rating when clicked
     let newRating = this.state.rating;
 
-    console.log(newRating);
+    this.setState({clicked: true});
 
+    //this allows you to return to 0 rating when clicked a second time
+    //currently if you hover over and then click, it goes down 1 star - attempting to fix
     if (nextValue === newRating) {
       newRating -= 1;
       this.setState({rating: newRating});
@@ -27,8 +30,14 @@ class RatingInput extends React.Component {
   }
 
   onStarHover(nextValue, prevValue, name) {
+    //this sets the rating on hovering over
     this.setState({rating: nextValue});
   }
+
+  /*I tried using the onStarHoverOut function but this just created more issues!!
+  Ideally once clicked, onStarHover would either stop working or when HoverOut it would revert
+  to previous setState
+  Any ideas on this? */
 
   render() {
     const { rating } = this.state;

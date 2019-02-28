88d53623703ee20b2e0c8954a876b5ff2bd08521
emmastockton <emmastockton10@gmail.com>
2019-02-28 19:19:47 +0000

    Film table added - not yet functioning

--- src/components/FilmListTable.js
+++ src/components/FilmListTable.js
@@ -1,6 +1,25 @@
 import React from 'react';
+import {BootstrapTable, TableHeaderColumn} from 'react-bootstrap-table';
 
 class FilmListTable extends React.Component {
+
+    render() {
+        return (
+          <div>
+            <BootstrapTable data={this.props.filmList}>
+              <TableHeaderColumn isKey dataField='filmTitle'>
+                Title
+              </TableHeaderColumn>
+              <TableHeaderColumn dataField='rating'>
+                Rating
+              </TableHeaderColumn>
+              <TableHeaderColumn dataField='genre'>
+                Genre
+              </TableHeaderColumn>
+            </BootstrapTable>
+          </div>
+        );
+      }
 
 
 }
